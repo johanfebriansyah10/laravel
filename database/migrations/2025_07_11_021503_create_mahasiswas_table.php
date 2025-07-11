a9238153dbbb039f2dbf6f22677fccddee68cce3
Johan <johanfebriansyah2022@gmail.com>
2025-07-11 11:50:46 +0800

    database setting

---
+++ database/migrations/2025_07_11_021503_create_mahasiswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->uniqie();
+            $table->string('nim', 7)->unique();
+            $table->string('nama', 50);
+            $table->string('tempat_larir', 50);
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin', 10);
+            $table->string('alamat', 100);
+            $table->string('prodi', 30);
+            $table->string('angkatan', 4);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
---
+++ database/migrations/2025_07_11_023500_create_dosens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->string('nidn', 7)->unique();
+            $table->string('nama', 50);
+            $table->string('gelar', 50)->nullable();
+            $table->string('jenis_kelamin', 10);
+            $table->string('email', 50)->uniqie();
+            $table->string('alamat', 70);
+            $table->string('no_hp', 20)->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dosens');
+    }
+};
